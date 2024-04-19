7a4b2ce94dd93a1d534416a383459ca8b7dc5f34
JoaoMaciel5G <juauvitu765@gmail.com>
2024-04-18 23:21:09 -0300

    feat: add component header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,20 +1,20 @@
-import Link from "next/link";
+import Links from "./Links";
+import { SquareTerminal } from "lucide-react";
 
 const Header = () => {
     return (
-        <header className="flex py-8 justify-between px-60">
+        <header className="flex py-8 justify-between">
             <div>
-                <h2 className="text-3xl text-blue-700 font-semibold">
-                    Joao Maciel
+                <h2 className="text-3xl text-blue-700 font-semibold flex items-center gap-2">
+                    <span><SquareTerminal size={35}/></span>
+                    João Maciel
                 </h2>
             </div>
             <nav className="flex gap-8">
-                <div>
-                    <Link href="/work" className="text-lg hover:opacity-70">Projetos</Link>
-                </div>
-                <div>
-                    <Link href="/about" className="text-lg hover:opacity-70">Sobre mim</Link>
-                </div>
+                <Links text="Home"/>
+                <Links text="Projetos" href="projects"/>
+                <Links text="Sobre mim" href="about"/>
+                <Links text="Techs" href="skills"/>
             </nav>
         </header>
     );
